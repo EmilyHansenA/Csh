fbb59155bb45ee06d7039649de1bae9842a798be
EmilyHansen <Lyaysan78@gmail.com>
2022-04-01 03:54:08 +0400

    Змейка без смертей

---
+++ Змейка без смертей/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Game_Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pcBox = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Candara", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnStart.Location = new System.Drawing.Point(250, 450);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(11, 11, 11, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 100);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.StartGame);
+            // 
+            // pcBox
+            // 
+            this.pcBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pcBox.Location = new System.Drawing.Point(12, 12);
+            this.pcBox.Name = "pcBox";
+            this.pcBox.Size = new System.Drawing.Size(660, 424);
+            this.pcBox.TabIndex = 1;
+            this.pcBox.TabStop = false;
+            this.pcBox.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePcBoxGraphics);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(473, 450);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(158, 48);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.Location = new System.Drawing.Point(50, 450);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(153, 48);
+            this.lblRecord.TabIndex = 2;
+            this.lblRecord.Text = "Record:";
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 40;
+            this.tmr.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 48F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.lblRecord);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pcBox);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(11, 11, 11, 11);
+            this.Name = "Form1";
+            this.Text = "Game_Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pcBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pcBox;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblRecord;
+        private System.Windows.Forms.Timer tmr;
+    }
+}
+
---
+++ Змейка без смертей/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Snake
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+
+        int maxWidth;
+        int maxHeight;
+
+        int Record;
+        int Score;
+
+        Random rand = new Random();
+
+        bool goLeft, goRight, goDown, goUp;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            new Settings();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && Settings.direction != "right")
+            {
+                goLeft = true;
+            }
+
+            if (e.KeyCode == Keys.Right && Settings.direction != "left")
+            {
+                goRight = true;
+            }
+
+            if (e.KeyCode == Keys.Down && Settings.direction != "Down")
+            {
+                goDown = true;
+            }
+
+            if (e.KeyCode == Keys.Up && Settings.direction != "up")
+            {
+                goUp = true;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (goLeft)
+            {
+                Settings.direction = ("left");
+            }
+            if (goRight)
+            {
+                Settings.direction = ("right");
+            }
+            if (goDown)
+            {
+                Settings.direction = ("down");
+            }
+            if (goUp)
+            {
+                Settings.direction = ("up");
+            }
+
+            for (int i = Snake.Count -1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (Settings.direction)
+                    {
+                        case "left" :
+                            Snake[i].X--;
+                            break;
+
+                        case "right":
+                            Snake[i].X++;
+                            break;
+
+                        case "down":
+                            Snake[i].Y++;
+                            break;
+
+                        case "up":
+                            Snake[i].Y--;
+                            break;
+                    }
+
+                    if (Snake[i].X < 0)
+                    {
+                        Snake[i].X = maxWidth;
+                    }
+                    if (Snake[i].X > maxWidth)
+                    {
+                        Snake[i].X = 0;
+                    }
+                    if (Snake[i].Y < 0)
+                    {
+                        Snake[i].Y = maxHeight;
+                    }
+                    if (Snake[i].Y > maxHeight)
+                    {
+                        Snake[i].Y = 0;
+                    }
+
+                    if (Snake[i].X == food.X && Snake[i].Y == food.Y)
+                    {
+                        EatFood();
+                    }
+                }
+                else
+                {
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+            pcBox.Invalidate();
+        }
+
+        private void UpdatePcBoxGraphics(object sender, PaintEventArgs e)
+        {
+            Graphics pcBox = e.Graphics;
+
+            Brush snakeColor;
+
+            for (int i = 0; i < Snake.Count; i++)
+            {
+                if (i == 0)
+                {
+                    snakeColor = Brushes.Black;
+                }
+                else
+                {
+                    snakeColor = Brushes.Green;
+                }
+
+                pcBox.FillEllipse(snakeColor, new Rectangle
+                    (
+                    Snake[i].X * Settings.Width,
+                    Snake[i].Y * Settings.Height,
+                    Settings.Width, Settings.Height
+                    ));
+            }
+
+
+            pcBox.FillEllipse(Brushes.Red, new Rectangle
+                (
+                food.X * Settings.Width,
+                food.Y * Settings.Height,
+                Settings.Width, Settings.Height
+                ));
+        }
+
+        private void RestartGame()
+        {
+            maxWidth = pcBox.Width / Settings.Width - 1;
+            maxHeight = pcBox.Height / Settings.Height - 1;
+
+            Snake.Clear();
+
+            btnStart.Enabled = false;
+
+            Score = 0;
+            lblScore.Text = "Score: " + Score;
+
+            Circle head = new Circle() { X = 10, Y = 5 };
+
+            Snake.Add(head);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Circle body = new Circle();
+                Snake.Add(body);
+            }
+
+            food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+
+            tmr.Start();
+        }
+
+        private void EatFood()
+        {
+            Score++;
+            lblScore.Text = "Score: " + Score;
+
+            Circle body = new Circle
+            {
+                X = Snake[Snake.Count - 1].X,
+                Y = Snake[Snake.Count - 1].Y
+            };
+
+            Snake.Add(body);
+
+            food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+        }
+
+        private void GameOver()
+        {
+
+        }
+    }
+}
