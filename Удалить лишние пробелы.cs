b38624b78f4a3b54241a3cb389cc6db3b8820a14
EmilyHansen <Lyaysan78@gmail.com>
2022-04-18 01:13:20 +0400

    Create Удалить лишние пробелы.cs

---
+++ Удалить лишние пробелы.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            while (s.Contains("  ")) {
+                s = s.Replace("  ", " "); 
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
