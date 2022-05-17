7a2b02028586da7653387e15f8cbb9aa65812341
EmilyHansen <Lyaysan78@gmail.com>
2022-05-17 21:07:38 +0400

    Анимация загрузки
    Написала анимацию загрузки, которая меняет свой цвет каждый отрисованный круг.

---
+++ AnimationTaskGL/Animation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimationTaskGL
+{
+    public partial class Animation : Form
+    {
+        public Animation()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            ClientSize = new Size(500, 500);
+
+            var centerX = ClientSize.Width / 2;
+            var centerY = ClientSize.Height / 2;
+
+            var size = 100;
+            var radius = Math.Min(ClientSize.Width, ClientSize.Height) / 3;
+
+            var time = 0;
+            var Timer = new Timer();
+            Timer.Interval = 500;
+            Timer.Tick += (sender, args) =>
+            {
+                time++;
+                Invalidate();
+            };
+
+            Timer.Start();
+
+            Paint += (sender, args) =>
+            {
+                Random rand = new Random();
+                SolidBrush brush = new SolidBrush(Color.FromArgb(255, (byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256)));
+
+                for (int i = 0; i < time; i++)
+                {
+                    args.Graphics.TranslateTransform(centerX, centerY);
+                    args.Graphics.RotateTransform(i * 360f / 10);
+                    args.Graphics.FillEllipse(brush, radius - size / 2, -size / 2, size, size);
+
+                    args.Graphics.ResetTransform();
+                }
+            };
+        }
+
+        private void Animation_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AnimationWPF/AnimationGL/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimationGL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.From = 50;
+            anim.To = 250;
+            anim.Duration = TimeSpan.FromSeconds(0.9);
+            anim.EasingFunction = new QuarticEase(); //Когда анимация подходит к концу — замедляется воспроизведение.
+            GreenButton.BeginAnimation(HeightProperty, anim);
+        }
+    }
+}
