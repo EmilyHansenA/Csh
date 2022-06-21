c23cceec4a200a4477c0c7a0bbda1d87695f73d8
Lyaysan Gimatdinova <39967814+EmilyHansenA@users.noreply.github.com>
2022-06-22 03:46:15 +0400

    Update Тернарный оператор + проверка на отрицательное число.cs

--- Functions/Тернарный оператор + проверка на отрицательное число.cs
+++ Functions/Тернарный оператор + проверка на отрицательное число.cs
@@ -4,3 +4,8 @@ string output = num < 0 ? "Negative. Number = " + Convert.ToString(num) :
     "Positive. Number = " + Convert.ToString(num);
 
 Console.WriteLine(output);
+
+/* 
+Определяет какое число. Отрицательное или положительное. 
+Выводит ответ и само число.
+*/
