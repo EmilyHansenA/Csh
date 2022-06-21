97a2c32cadcd735e93e19c061c37a713fb87b284
Lyaysan Gimatdinova <39967814+EmilyHansenA@users.noreply.github.com>
2022-06-22 03:41:38 +0400

    Update Powers of 2.cs

--- Functions/Powers of 2.cs
+++ Functions/Powers of 2.cs
@@ -8,3 +8,11 @@ public class Kata
         return Enumerable.Range(0, ++n).Select(num => new BigInteger(Math.Pow(2, num))).ToArray();
     }
 }
+
+/*
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+Инициализирует массив степенями двойки.
+*/
