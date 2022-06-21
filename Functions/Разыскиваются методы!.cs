ff086faaf1cac4b83a44ff0054304ccce07d6373
EmilyHansen <Lyaysan78@gmail.com>
2022-06-21 21:06:51 +0400

    Create Разыскиваются методы!.cs

---
+++ Functions/Разыскиваются методы!.cs
@@ -0,0 +1,10 @@
+static string GetLastHalf(string text)
+{
+    text = text.Substring(text.Length / 2);
+    string s = text.Replace(" ", String.Empty);
+    return s;
+}
+
+Console.WriteLine(GetLastHalf("I love CSharp!"));
+Console.WriteLine(GetLastHalf("1234567890"));
+Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
