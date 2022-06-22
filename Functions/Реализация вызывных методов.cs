fda84101f74783fef78fe61d3f96bb751b040e4f
EmilyHansen <Lyaysan78@gmail.com>
2022-06-22 06:09:19 +0400

    Create Реализация вызывных методов.cs

---
+++ Functions/Реализация вызывных методов.cs
@@ -0,0 +1,16 @@
+﻿static int SumNums(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+static void PrintResult(int result)
+{
+    Console.WriteLine("Result num1 + num2 = " + result);
+}
+
+int result;
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+result = SumNums(num1, num2);
+PrintResult(result);
