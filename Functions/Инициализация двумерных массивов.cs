e7a6d60c5e074104541ac22ed034ead5da875e39
EmilyHansen <Lyaysan78@gmail.com>
2022-06-22 05:54:18 +0400

    Update Инициализация двумерных массивов.cs

--- Functions/Инициализация двумерных массивов.cs
+++ Functions/Инициализация двумерных массивов.cs
@@ -7,12 +7,9 @@ int[,] array1 = new int[2, 2]
     {1, 3},
 };
 
-int height = array1.GetLength(0);
-int width = array1.GetLength(1);
-
-for (int i = 0; i < height; i++)
+for (int i = 0; i < array1.GetLength(0); i++)
 {
-    for (int j = 0; j < width; j++)
+    for (int j = 0; j < array1.GetLength(1); j++)
     {
         Console.Write(array1[i, j] + "\t");
     }
@@ -24,17 +21,17 @@ Console.WriteLine(Environment.NewLine);
 #region Инициализация двумерного массива рандомно
 int[,] array2 = new int[2, 2];
 
-for (int i = 0; i < height; i++)
+for (int i = 0; i < array2.GetLength(0); i++)
 {
-    for (int j = 0; j < width; j++)
+    for (int j = 0; j < array2.GetLength(1); j++)
     {
         array2[i, j] = random.Next(100);
     }
 }
 
-for (int i = 0; i < height; i++)
+for (int i = 0; i < array2.GetLength(0); i++)
 {
-    for (int j = 0; j < width; j++)
+    for (int j = 0; j < array2.GetLength(1); j++)
     {
         Console.Write(array2[i, j] + "\t");
     }
